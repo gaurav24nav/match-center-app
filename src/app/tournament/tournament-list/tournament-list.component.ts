c603679fbccf71bd1ea0eb2734acbc1d3c3477bb
Hemant Patidar <hemant.eblue@gmail.comls>
2019-06-11 19:56:39 +0530

    Page UI Upodated

--- src/app/tournament/tournament-list/tournament-list.component.ts
+++ src/app/tournament/tournament-list/tournament-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-tournament-list',
@@ -10,6 +11,12 @@ export class TournamentListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $('.sub-menu ul').hide();
+$(".sub-menu a").click(function () {
+	$(this).parent(".sub-menu").children("ul").slideToggle("100");
+	$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+});
   }
 
 }
